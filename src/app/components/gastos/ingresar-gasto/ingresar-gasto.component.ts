a355181f7ae297609d6b5c03f9ec2a0fecb68eb1
NuPaCoding <nupacoding@gmail.com>
2022-09-18 21:00:43 -0300

    ingresar gasto 01

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingresar-gasto.component.css']
 })
 export class IngresarGastoComponent implements OnInit {
-
-  constructor() { }
+  nombreGasto: string;
+  cantidad:number; 
+  formularioIncorrecto:boolean;
+  textoIncorrecto:string;
+  constructor() {
+    this.nombreGasto = "";
+    this.cantidad = 0;
+    this.formularioIncorrecto = false;
+    this.textoIncorrecto = "Nombre del gasto o cantidad incorrecta";
+   }
 
   ngOnInit(): void {
+  }
+
+  agregarGastos(){
+    if(this.nombreGasto=== "" || this.cantidad <= 0){
+      this.formularioIncorrecto = true;
+     } else {
+    this.formularioIncorrecto = false;
+    this.nombreGasto = "";
+    this.cantidad = 0;
+    }
   }
 
 }
